5b8b073f1baf1106b1b20e110dfb1ca2efa3bffc
eufelipepinheiro <pinheirofelipe1248@gmail.com>
2026-03-08 11:01:22 -0300

    feat: criação do service de Paciente

---
+++ backend-consultas/src/main/java/com/fiap/ec/backend_consultas/service/PacienteService.java
@@ -0,0 +1,30 @@
+package com.fiap.ec.backend_consultas.service;
+
+import com.fiap.ec.backend_consultas.model.Paciente;
+import com.fiap.ec.backend_consultas.repository.PacienteRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PacienteService {
+
+    private final PacienteRepository repository;
+
+    public PacienteService(PacienteRepository repository) {
+        this.repository = repository;
+    }
+
+    public Paciente salvar(Paciente paciente) {
+        return repository.save(paciente);
+    }
+
+    public List<Paciente> listar() {
+        return repository.findAll();
+    }
+
+    public Optional<Paciente> buscarPorId(Long id) {
+        return repository.findById(id);
+    }
+}
